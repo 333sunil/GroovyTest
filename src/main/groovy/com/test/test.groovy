37f0b7711bb64dc76ce8cee00e6e1701ca966c88
333sunil <hullursunil@gmail.com>
2021-04-24 19:40:48 +0530

    Update test.groovy

--- src/main/groovy/com/test/test.groovy
+++ src/main/groovy/com/test/test.groovy
@@ -4,6 +4,13 @@ class test{
 
 static void main(args){
 def env = System.getenv()
-println env.testvalue
+List<String> taxonomy_list = new ArrayList<>()
+String taxonomy_string = env.TAXONOMIES as String
+String[] taxonomy_array = taxonomy_string.split(",")
+for( String value : taxonomy_array )
+  {
+    taxonomy_list.add(value)
+  }
+ println taxonomy_list 
 }
 }
