d9aad42392a5f36523e116d7fe98879e34c71530
333sunil <hullursunil@gmail.com>
2021-04-24 17:09:42 +0530

    Update test.groovy

--- src/main/groovy/com/test/test.groovy
+++ src/main/groovy/com/test/test.groovy
@@ -1,17 +1,5 @@
 package com.test
 
-import hudson.model.*
- 
-// get current thread / Executor
-def thr = Thread.currentThread()
-// get current build
-def build = thr?.executable
- 
- 
-// get parameters
-def parameters = build?.actions.find{ it instanceof ParametersAction }?.parameters
-parameters.each {
-   println "parameter ${it.name}:"
-   println it.dump()
-   println "-" * 80
+def printparams(value){
+ println value
 }
