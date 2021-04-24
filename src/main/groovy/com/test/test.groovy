5f0f46f80c81a17c76e3de1ea01222e710ff26dc
333sunil <hullursunil@gmail.com>
2021-04-24 17:07:21 +0530

    Update test.groovy

--- src/main/groovy/com/test/test.groovy
+++ src/main/groovy/com/test/test.groovy
@@ -1,6 +1,17 @@
 package com.test
 
-println('Starting DataCollection')
-println('Args '+ args)
-def value = "\"${testvalue}\""
-println value
+import hudson.model.*
+ 
+// get current thread / Executor
+def thr = Thread.currentThread()
+// get current build
+def build = thr?.executable
+ 
+ 
+// get parameters
+def parameters = build?.actions.find{ it instanceof ParametersAction }?.parameters
+parameters.each {
+   println "parameter ${it.name}:"
+   println it.dump()
+   println "-" * 80
+}
