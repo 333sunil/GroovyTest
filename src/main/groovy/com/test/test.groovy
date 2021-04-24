a9048b69398a9ac4341cedf8c6f350f14d2afa8b
333sunil <hullursunil@gmail.com>
2021-04-24 18:30:10 +0530

    Update test.groovy

--- src/main/groovy/com/test/test.groovy
+++ src/main/groovy/com/test/test.groovy
@@ -2,17 +2,7 @@ package com.test
 
 import hudson.model.*
 
-// Get the out variable
-def out = getBinding().out;
-
-class OutputClass
-{
-    OutputClass(out)  // Have to pass the out variable to the class
-    {
-        out.println ("Inside class")
-        out.println ("${WORKSPACE}")
-    }
+def env = System.getenv()
+env.each{
+println it
 }
-
-out.println("Outside class")
-output = new OutputClass(out)
